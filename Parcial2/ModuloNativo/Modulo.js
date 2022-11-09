9bb346758a94c9a27b03c2656ab8284543920977
SebasMag123 <sebmag2011@hotmail.com>
2022-11-09 11:10:45 -0600

    Enter

---
+++ Parcial2/ModuloNativo/Modulo.js
@@ -0,0 +1,9 @@
+/*Modulo: archivo que exporta funcionalidad*/
+export function mayusculas (cad){
+    return cad.toUpperCase();
+}
+
+export function longitud(cad){
+    return cad.length;
+}
+/*AMD: Asynchronous Module Definition */
---
+++ Parcial3/ModuloCommonJS/App.js
@@ -0,0 +1,10 @@
+const Modulo= require("./Modulo.js");
+
+console.log(Modulo.cuadrado(20));
+
+var cowsay=require("cowsay");
+console.log(cowsay.say)({
+    text:"Mensaje",
+    e:"oO",
+    T:"UU"
+});
