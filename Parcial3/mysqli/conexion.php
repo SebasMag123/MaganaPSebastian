5170bb6bc516b2d6ef4110e14fd79eb35c90fa92
SebasMag123 <sebmag2011@hotmail.com>
2022-11-19 20:58:14 -0600

    Enter

--- Parcial3/mysqli/conexion.php
+++ Parcial3/mysqli/conexion.php
@@ -1,11 +1,11 @@
 <?php
 $servidor="localhost";
-$basedatos="BaseFormulario";
+$basedatos="baseproyecto";
 $usuario="root";
-$password="";
+$password="root";
 
 $con =mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar a local host");
-$consulta="select * from Empleado";
+$consulta="select * from avion";
 $registros=mysqli_connect($con,$consulta) or die("Problemas en el select");
 $result=mysqli_fetch_all($registros);
 mysqli_close($con);
