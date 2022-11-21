407cb2d4e2f662185d98d0ee138e98cacc76b1ee
SebasMag123 <sebmag2011@hotmail.com>
2022-11-20 22:46:40 -0600

    Enter

--- Parcial3/mysqli/conexion.php
+++ Parcial3/mysqli/conexion.php
@@ -1,13 +1,16 @@
 <?php
-$servidor="localhost";
-$basedatos="baseproyecto";
+$servidor= "localhost";
+$basedatos= "baseproyecto";
 $usuario="root";
 $password="root";
 
-$con =mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar a local host");
-$consulta="select * from avion";
-$registros=mysqli_connect($con,$consulta) or die("Problemas en el select");
-$result=mysqli_fetch_all($registros);
+$con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se pudo conectar a localhost");
+$consulta = "select * from avion";
+$registros = mysqli_query($con,$consulta) or die("Problemas en el select");
+
+$result = mysqli_fetch_all($registros);
+
 mysqli_close($con);
 echo json_encode($result);
+
 ?>
