e74bfedf9f0593f62ea16f4b641f79edb90db454
SebasMag123 <sebmag2011@hotmail.com>
2022-12-02 18:33:59 -0600

    Enter

--- Parcial3/Sesiones/Pagina.php
+++ Parcial3/Sesiones/Pagina.php
@@ -14,35 +14,6 @@
 </head>
 <body>
   <h1>Pagina principal de la aplicacion</h1>
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
     <button type="button"><?php echo $_SESSION['usuario'];?></button>
 </body>
 </html>
