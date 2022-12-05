61e2698958e5e99e8443b8804cdc18478109275c
SebasMag123 <sebmag2011@hotmail.com>
2022-12-04 21:18:08 -0600

    Enter

--- Parcial3/PDF/PDF.php
+++ Parcial3/PDF/PDF.php
@@ -1,9 +1,49 @@
 <?php
 require('./FPDF/fpdf.php');
+$nombre= $_POST['nombre'];
 
-$pdf = new FPDF();
+$apellidos =$_POST['apellidos'];
+
+$sueldo = $_POST['sueldo'];
+
+$puesto = $_POST['puesto'];
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Movernos a la derecha
+    $this->Cell(80);
+    // Título
+    $this->Cell(60,20,'Reporte de empleado',1,0,'C');
+    // Salto de línea
+    $this->Ln(20);
+}
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+// Creación del objeto de la clase heredada
+$pdf = new PDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!');
+$pdf->SetFont('Arial','',12);
+$pdf->Cell(40,10,"Nombre:"." ".$nombre." ".$apellidos,2,'C');
+$pdf->Ln();
+$pdf->Cell(40,10,"Sueldo:"." ".$sueldo,2,'C');
+$pdf->Ln();
+$pdf->Cell(40,10,"Puesto:"." ".$puesto,2,'C');
 $pdf->Output();
 ?>
